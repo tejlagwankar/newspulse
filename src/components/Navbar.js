3aee2e9a112bc952d22d3ebce6b321cbfc738531
Tejas Lagwankar <tejlagwankar1999@gmail.com>
2023-10-06 21:40:07 +0530

    Minor Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class Navbar extends Component {
@@ -9,7 +8,7 @@ export class Navbar extends Component {
             <>
                 <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
+                        <a className="navbar-brand" href="/">NewsPulse</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
